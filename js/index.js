f63edbb513a6d9ae4208179f53606c14511c8c42
Mateusz <galganek.mateusz@gmail.com>
2016-03-30 22:14:11 +0200

    Refactor

--- js/index.js
+++ js/index.js
@@ -9,26 +9,6 @@ config = function(){
   }
 }();
 
-function dotClicked(e) {
-  let element = e.target;
-  if (!(($(element).hasClass('empty')) || $(element).hasClass('empty'))) {
-    config.ATTEMPTS -= 1;
-  }
-
-  if (config.ATTEMPTS > 0) {
-    if ($(element).data('win') === true) {
-      $(element).addClass("found");
-      config.area.gameOver('win');
-    } else {
-      $(element).addClass("empty");
-    }
-  } else if (config.ATTEMPTS == 0) {
-    $(element).addClass("empty");
-    config.area.gameOver('lose');
-  }
-  $('#attempts').text(config.ATTEMPTS);
-}
-
 $(document).ready(function() {
   $('#attempts').text(config.ATTEMPTS);
   config.area = new Area(config.COLUMNS, config.ROWS, config.ATTEMPTS);
@@ -40,6 +20,25 @@ class Dot{
     this.id = `point-row${row}-col${column}`;
     this.column = column;
     this.row = row;
+  }
+  dotClicked(e) {
+    let element = e.target;
+    if (!(($(element).hasClass('empty')) || $(element).hasClass('empty'))) {
+      config.ATTEMPTS -= 1;
+    }
+
+    if (config.ATTEMPTS > 0) {
+      if ($(element).data('win') === true) {
+        $(element).addClass("found");
+        config.area.gameOver('win');
+      } else {
+        $(element).addClass("empty");
+      }
+    } else if (config.ATTEMPTS == 0) {
+      $(element).addClass("empty");
+      config.area.gameOver('lose');
+    }
+    $('#attempts').text(config.ATTEMPTS);
   }
 }
 
@@ -64,7 +63,7 @@ class Area{
       let shape = shapes[i];
       let newElement = $('<div>').attr(
         {id: shape.id}
-      ).addClass("circle").data('win', false);
+      ).addClass("circle").data('win', false).on('click', shape.dotClicked);
       $('#area').append(newElement);
     }
   }
@@ -77,7 +76,7 @@ class Area{
     let allCircles = $('.circle');
     let winCircle = Math.floor((Math.random() * allCircles.length));
     $(allCircles[winCircle]).data('win', true);
-    $('.circle').on('click', dotClicked);
+
   }
 
   gameOver(state) {
